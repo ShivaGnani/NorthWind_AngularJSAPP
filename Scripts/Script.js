2511a6d1c7dc760c8ffc7f69fb2e9cee3c076edc
Shiva Gnani <shivagnani@gmail.com>
2017-03-31 15:39:56 -0400

    second commit

---
+++ Scripts/Script.js
@@ -0,0 +1,49 @@
+﻿/// <reference path="angular.js" />
+var app = angular.module("myApp", [])
+
+    .factory("apiCallFactory", function ($http, $q) {
+        var deferred = $q.defer();
+        function getCategories() {
+            $http.get('http://localhost/NorthWind_WebAPI/api/Order/Categories')
+            .then(function (response) {
+                deferred.resolve(response.data);
+            })
+            return deferred.promise;
+
+        }
+        function getProductsByID(categoryID) {
+            var deferred = $q.defer();
+            $http({
+                url: 'http://localhost/NorthWind_WebAPI/api/Order/Productsbyid',
+                method: 'GET',
+                params: { id: categoryID }
+            }).then(function (response) {
+                deferred.resolve(response.data);
+            })
+            return deferred.promise;
+        }
+        return {
+            getCategories: getCategories,
+            getProductsByID: getProductsByID
+        }
+    })
+
+    .controller("orderCtrl", function ($scope, apiCallFactory) {
+        apiCallFactory.getCategories()
+                .then(function (res) {
+                    $scope.categories = res;
+                })
+        $scope.getProductsByID = function (categoryID) {
+            apiCallFactory.getProductsByID(categoryID)
+            .then(function (res) {
+                $scope.products = res;
+            })
+            $scope.calPrice = function () {
+                $scope.unitPrice = $scope.selectedProduct.UnitPrice;
+                $scope.totPrice = $scope.unitPrice * $scope.quantity;
+            }
+            //$scope.calTotPrice = function () {
+            //    $scope.totPrice = $scope.unitPrice * $scope.quantity;
+            //}
+        }
+});
